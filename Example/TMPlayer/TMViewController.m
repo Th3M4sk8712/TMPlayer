bc9f691dc9417c36ecfa310bb5abe835aade9827
Th3M4sk <themaskhenzo@gmail.com>
2020-02-02 13:55:12 +0700

    update version 1.2.1

--- Example/TMPlayer/TMViewController.m
+++ Example/TMPlayer/TMViewController.m
@@ -8,11 +8,16 @@
 
 #import "TMViewController.h"
 #import "ZFPlayer.h"
+#import "ZFAVPlayerManager.h"
+#import "ZFPlayerControlView.h"
+#import "UIView+ZFFrame.h"
 
 @interface TMViewController ()
 
-@property (weak, nonatomic)  IBOutlet UIView *playerFatherView;
-@property (strong, nonatomic) ZFPlayerView *playerView;
+@property (weak, nonatomic)  IBOutlet UIView *containerView;
+
+@property (nonatomic, strong) ZFPlayerController *player;
+@property (nonatomic, strong) ZFPlayerControlView *controlView;
 
 @end
 
@@ -21,26 +26,76 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    self.playerView = [[ZFPlayerView alloc] init];
-    ZFPlayerModel *playerModel = [[ZFPlayerModel alloc] init];
-    playerModel.fatherView = _playerFatherView;
-    playerModel.videoURL = [NSURL URLWithString:@"http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"];
-    playerModel.title = @"Big Buck Bunny";
-    self.playerView.hasDownload = NO;
-    self.playerView.hasPreviewView = YES;
-    [self.playerView playerControlView: nil playerModel:playerModel];
+//    self.playerView = [[ZFPlayerView alloc] init];
+//    ZFPlayerModel *playerModel = [[ZFPlayerModel alloc] init];
+//    playerModel.fatherView = _playerFatherView;
+//    playerModel.videoURL = [NSURL URLWithString:@"http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"];
+//    playerModel.title = @"Big Buck Bunny";
+//    self.playerView.hasDownload = NO;
+//    self.playerView.hasPreviewView = YES;
+//    [self.playerView playerControlView: nil playerModel:playerModel];
     //[self.playerView addSubtitleURL:@"..."];
     //[self.playerView resetToPlayNewVideo:playerModel];
+  
+  [self initPlayerView];
+  self.player.forceDeviceOrientation = YES;
+}
+
+- (void)initPlayerView {
+  ZFAVPlayerManager *playerManager = [[ZFAVPlayerManager alloc] init];
+  self.player = [[ZFPlayerController alloc] initWithPlayerManager:playerManager containerView:self.containerView];
+  self.player.controlView = self.controlView;
+  @weakify(self)
+  self.player.orientationWillChange = ^(ZFPlayerController * _Nonnull player, BOOL isFullScreen) {
+      @strongify(self)
+      [self setNeedsStatusBarAppearanceUpdate];
+  };
+  
+  NSString *URLString = [@"http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+  playerManager.assetURL = [NSURL URLWithString:URLString];
+  
+  [self.controlView showTitle:@"Big Buck Bunny" coverURLString:@"https://upload-images.jianshu.io/upload_images/635942-14593722fe3f0695.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240" fullScreenMode:ZFFullScreenModeLandscape];
+}
+
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    if (self.player.isFullScreen) {
+        return UIStatusBarStyleLightContent;
+    }
+    return UIStatusBarStyleDefault;
+}
+
+- (BOOL)prefersStatusBarHidden {
+    return self.player.isStatusBarHidden;
+}
+
+- (UIStatusBarAnimation)preferredStatusBarUpdateAnimation {
+  return UIStatusBarAnimationSlide;
 }
 
 - (BOOL)shouldAutorotate {
-    return NO;
+    return self.player.shouldAutorotate;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - about keyboard orientation
+
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
+    if (self.player.isFullScreen) {
+        return UIInterfaceOrientationMaskLandscape;
+    }
+    return UIInterfaceOrientationMaskPortrait;
+}
+
+- (ZFPlayerControlView *)controlView {
+    if (!_controlView) {
+        _controlView = [ZFPlayerControlView new];
+    }
+    return _controlView;
 }
 
 @end
--- TMPlayer.podspec
+++ TMPlayer.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'TMPlayer'
-  s.version          = '1.1.9'
+  s.version          = '1.2.1'
   s.summary          = 'A perfect player based on ZFPlayer'
 
 # This description is used to generate tags and improve search results.
@@ -28,7 +28,7 @@ A perfect player based on ZFPlayer made by renzifeng, extend by Nguyễn Khánh 
   s.source           = { :git => 'https://github.com/Th3M4sk8712/TMPlayer.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-  s.ios.deployment_target = '8.0'
+  s.ios.deployment_target = '10.0'
 
   s.source_files = 'TMPlayer/**/*.{h,m}'
   s.resources = 'TMPlayer/ZFPlayer.bundle'
